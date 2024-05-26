22a792112ef7ae688cd8ed1468509e368d59236a
Chanrith Natvaddhtharo <87104253+TharoChan@users.noreply.github.com>
2024-05-26 14:29:22 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,41 +1,47 @@
 // create web server
-// http://localhost:3000
-// http://localhost:3000/comments
-// http://localhost:3000/comments/1
-// http://localhost:3000/comments/2
-// http://localhost:3000/comments/3
-// http://localhost:3000/comments/4
-// http://localhost:3000/comments/5
-// http://localhost:3000/comments/6
-// http://localhost:3000/comments/7
-// http://localhost:3000/comments/8
-// http://localhost:3000/comments/9
-// http://localhost:3000/comments/10
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
-var http = require('http');
-var url = require('url');
+// use body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-var comments = [
-  {name: 'John', comment: 'Hello'},
-  {name: 'Jane', comment: 'World'},
-  {name: 'Jim', comment: 'JavaScript'},
-  {name: 'Jack', comment: 'Node.js'},
-  {name: 'Jill', comment: 'Hello World'},
-  {name: 'Joe', comment: 'Hello JavaScript'},
-  {name: 'Jenny', comment: 'Hello Node.js'},
-  {name: 'Jerry', comment: 'Hello World'},
-  {name: 'Jesse', comment: 'Hello JavaScript'},
-  {name: 'Jasmine', comment: 'Hello Node.js'}
-];
-
-var server = http.createServer(function(request, response) {
-  var path = url.parse(request.url).pathname;
-  var index = parseInt(path.slice(9));
-  var comment = comments[index];
-
-  response.writeHead(200, {'Content-Type': 'application/json'});
-  response.end(JSON.stringify(comment));
+// get all comments
+app.get('/api/comments', (req, res) => {
+    fs.readFile('./comments.json', 'utf-8', (err, data) => {
+        if (err) {
+            res.status(500).send('Internal server error');
+        } else {
+            const comments = JSON.parse(data);
+            res.json(comments);
+        }
+    });
 });
 
-server.listen(3000, 'localhost');
-console.log('Server running at http://localhost:3000/');
+// add new comment
+app.post('/api/comments', (req, res) => {
+    const newComment = req.body;
+    fs.readFile('./comments.json', 'utf-8', (err, data) => {
+        if (err) {
+            res.status(500).send('Internal server error');
+        } else {
+            const comments = JSON.parse(data);
+            comments.push(newComment);
+            fs.writeFile('./comments.json', JSON.stringify(comments), (err) => {
+                if (err) {
+                    res.status(500).send('Internal server error');
+                } else {
+                    res.status(201).send('Comment added');
+                }
+            });
+        }
+    });
+});
+
+// start app
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+});
